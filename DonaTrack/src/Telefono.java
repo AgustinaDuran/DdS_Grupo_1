0626d1c27c2260cdf3e1111273b8b12f6e14ae0c
Daguerosilva <daguerosilva@frba.utn.edu.ar>
2026-05-21 14:57:14 -0300

    corrijo telefono

--- DonaTrack/src/Telefono.java
+++ DonaTrack/src/Telefono.java
@@ -2,4 +2,9 @@ public class Telefono extends Contacto {
     
     private String nroTelefono;
 
+    public Telefono(String telefono) {
+        super();
+        this.nroTelefono= telefono;
+    }
+
 }
