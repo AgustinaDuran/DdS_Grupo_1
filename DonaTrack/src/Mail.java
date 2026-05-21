356bb313caeb9c8a8b56d22f31625beb4f301690
Daguerosilva <daguerosilva@frba.utn.edu.ar>
2026-05-21 14:54:46 -0300

    corrijo mail

--- DonaTrack/src/Mail.java
+++ DonaTrack/src/Mail.java
@@ -2,4 +2,13 @@ public class Mail extends Contacto {
     
     private String direccionMail;
 
+    public Mail(String email) {
+        super();
+        direccionMail = email;
+    }
+
+    public String getDireccionMail() {
+        return direccionMail;
+    }
+
 }
