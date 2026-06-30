8090147679f539a4a8cb6df9439e7caa785739b4
AgustinaDuran <aduran@frba.utn.edu.ar>
2026-06-30 15:28:12 -0300

    cosas de mas

--- DonaTrack/servicio-logistica/src/main/java/org/donatrack/model/Chofer.java
+++ DonaTrack/servicio-logistica/src/main/java/org/donatrack/model/Chofer.java
@@ -25,12 +25,6 @@ public class Chofer {
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    public String getApellido() { return apellido; }
-    public void setApellido(String apellido) { this.apellido = apellido; }
-    public String getDocumento() { return documento; }
-    public void setDocumento(String documento) { this.documento = documento; }
     public Camion getCamion() { return camion; }
     public void setCamion(Camion camion) { this.camion = camion; }
 }
